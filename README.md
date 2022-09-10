aedf39b157c87c748f632b2345fd10590f060719
SyndiShanX <brianmatthew.1026@gmail.com>
2022-09-10 10:36:37 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Minecraft-Isometric-Script
-Generates an Isometric Image of a Block using a Given Texture
+
+Generates an Isometric Image of a Block using Blender
+
+## To use:
+
+* Replace `Texture.png` with your Block Texture > Run `Render_ISO.exe` to Create `Isometric.png`
---
+++ Render_ISO.py
@@ -0,0 +1,15 @@
+import subprocess, os
+from os import path
+
+fileDir = os.getcwd()
+fileDir = fileDir.replace('\\', '/')
+fileDir = fileDir + '/'
+
+fileDir = fileDir + "Texture.png"
+
+if os.path.exists('Isometric.png'):
+  os.remove('Isometric.png')
+
+subprocess.call('blender -b Isometric_Block.blend -o ' + fileDir + ' -f 1')
+
+os.rename('Texture.png0001.png','Isometric.png')
